b3cc0f8f077a7e3cdebfd44f1902f7202ec393c8
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-03 13:39:45 -0600

    create header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import Signin from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/about";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/Profile" element={<Profile />} />
+       <Route path="/about" element={<About />} /> 
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function about() {
   return (
-    <div>about</div>
+    <div>About</div>
   )
 }
