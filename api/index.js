a80023534006b89f0c1e727ef25bba9c6f413db7
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-04 17:54:16 -0600

    Created user update Api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
