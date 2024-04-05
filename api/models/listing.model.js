8aaa3d7ad51caadce5099ad00db35ed9d82e525d
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-04 18:52:21 -0600

    listing api rout

---
+++ api/models/listing.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
