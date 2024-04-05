b5b0e78b133dbb1afe0db92b652b1a19d72494b2
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-04 18:23:36 -0600

    add delete user function

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -35,3 +35,15 @@ export const test = (req, res) => {
       next(error);
     }
   };
+  
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only delete your own account!'));
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.clearCookie('access_token');
+      res.status(200).json('User has been deleted!');
+    } catch (error) {
+      next(error);
+    }
+  };
