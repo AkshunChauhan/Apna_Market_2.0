de1eebf1566442a5bc8d366ae419456bc4b61aeb
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-04-04 20:42:44 -0600

    add slider on listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
        <Route path="/about" element={<About />} /> 
        <Route element={<PrivateRoute />}>
+       <Route path='/listing/:listingId' element={<Listing />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route
